13ad498d2b2919bbf44a1ce7bd54a7524d3bad2f
David <david.507@yahoo.com>
2020-04-26 13:08:52 +0200

    add /me endpoint

--- web/src/main/java/org/david/sec/controller/UserController.java
+++ web/src/main/java/org/david/sec/controller/UserController.java
@@ -4,13 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.david.sec.dto.CreateUserDTO;
 import org.david.sec.dto.GetUserDTO;
 import org.david.sec.dto.converter.UserDtoConverter;
+import org.david.sec.model.UserEntity;
 import org.david.sec.service.UserEntityService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -24,5 +23,10 @@ public class UserController {
     public ResponseEntity<GetUserDTO> createUser(@RequestBody CreateUserDTO user) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(converter.convert(service.createUser(user)));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<GetUserDTO> me(@AuthenticationPrincipal UserEntity user) {
+        return ResponseEntity.status(HttpStatus.OK).body(converter.convert(user));
     }
 }
